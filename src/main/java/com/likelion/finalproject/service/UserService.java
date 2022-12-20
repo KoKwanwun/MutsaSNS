a4b949d0b8d9aea2573bf3a328b6ead02377483e
KoKwanwun <dkfajflsla@naver.com>
2022-12-20 16:36:04 +0900

    [Update] UserService : join 기능 추가

--- src/main/java/com/likelion/finalproject/service/UserService.java
+++ src/main/java/com/likelion/finalproject/service/UserService.java
@@ -1,7 +1,10 @@
 package com.likelion.finalproject.service;
 
+import com.likelion.finalproject.domain.User;
 import com.likelion.finalproject.domain.dto.UserDto;
 import com.likelion.finalproject.domain.dto.UserJoinRequest;
+import com.likelion.finalproject.exception.ErrorCode;
+import com.likelion.finalproject.exception.UserException;
 import com.likelion.finalproject.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -13,6 +16,18 @@ public class UserService {
     private final UserRepository userRepository;
 
     public UserDto join(UserJoinRequest userJoinRequest) {
+        // userName 중복여부 확인
+        userRepository.findByUserName(userJoinRequest.getUserName())
+                .ifPresent(user -> {
+                    throw new UserException(ErrorCode.DUPLICATED_USER_NAME, ErrorCode.DUPLICATED_USER_NAME.getMessage());
+                });
 
+        User savedUser = userRepository.save(userJoinRequest.toEntity());
+
+        return UserDto.builder()
+                .id(savedUser.getId())
+                .userName(savedUser.getUserName())
+                .password(savedUser.getPassword())
+                .build();
     }
 }
