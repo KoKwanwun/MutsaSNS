bdc9a1cf4c33e28d905c64b4a7723dba113d1a21
KoKwanwun <dkfajflsla@naver.com>
2022-12-20 16:09:50 +0900

    [Add] 회원가입, 로그인을 위한 domain 추가

---
+++ src/main/java/com/likelion/finalproject/domain/User.java
@@ -0,0 +1,23 @@
+package com.likelion.finalproject.domain;
+
+import lombok.*;
+
+import javax.persistence.*;
+
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Builder
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(unique = true)
+    private String userName;
+    private String password;
+
+    @Enumerated(EnumType.STRING)
+    private UserRole role;
+}
