c16c0ca5f16a429970aceff827f8d21474e741fd
KoKwanwun <dkfajflsla@naver.com>
2022-12-20 16:35:44 +0900

    [Update] User Entity 생성을 위한 toEntity 메소드 추가

--- src/main/java/com/likelion/finalproject/domain/dto/UserJoinRequest.java
+++ src/main/java/com/likelion/finalproject/domain/dto/UserJoinRequest.java
@@ -1,5 +1,7 @@
 package com.likelion.finalproject.domain.dto;
 
+import com.likelion.finalproject.domain.User;
+import com.likelion.finalproject.domain.UserRole;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 
@@ -8,4 +10,12 @@ import lombok.Getter;
 public class UserJoinRequest {
     private String userName;
     private String password;
+
+    public User toEntity(){
+        return User.builder()
+                .userName(this.userName)
+                .password(this.password)
+                .role(UserRole.USER)
+                .build();
+    }
 }
