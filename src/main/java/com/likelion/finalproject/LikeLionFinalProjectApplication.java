462f492209664584e7584b2a59ec796bf540dc8c
KoKwanwun <dkfajflsla@naver.com>
2022-12-20 10:53:07 +0900

    [Add] Project Build

---
+++ src/main/java/com/likelion/finalproject/LikeLionFinalProjectApplication.java
@@ -0,0 +1,13 @@
+package com.likelion.finalproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LikeLionFinalProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LikeLionFinalProjectApplication.class, args);
+    }
+
+}
