2b5ac4fc9b9dd8960ef5c6f0403d54e0714fa7e8
KoKwanwun <dkfajflsla@naver.com>
2022-12-21 15:32:21 +0900

    [Update] PostController : Post 리스트, 수정, 삭제 템플릿 추가

--- src/main/java/com/likelion/finalproject/controller/PostController.java
+++ src/main/java/com/likelion/finalproject/controller/PostController.java
@@ -9,17 +9,33 @@ import org.springframework.web.bind.annotation.*;
 @RequestMapping("/api/v1/posts")
 public class PostController {
 
-    @ApiOperation(value = "포스트 등록")
-    @PostMapping
-    public String registerPost(@RequestBody PostRegisterRequest postRegisterRequest){
-
-
-        return "Post Test";
+    @ApiOperation(value = "포스트 리스트")
+    @GetMapping()
+    public String printPosts() {
+        return "";
     }
 
     @ApiOperation(value = "포스트 상세")
     @GetMapping(value = "/{postsId}")
     public String printOnePost(@PathVariable Long postsId) {
         return "";
+    }
+
+    @ApiOperation(value = "포스트 등록")
+    @PostMapping
+    public String registerPost(@RequestBody PostRegisterRequest postRegisterRequest){
+        return "Post Register";
+    }
+
+    @ApiOperation(value = "포스트 수정")
+    @PutMapping("/{id}")
+    public String updatePost(@PathVariable Long id){
+        return "Post Update";
+    }
+
+    @ApiOperation(value = "포스트 삭제")
+    @DeleteMapping("/{id}")
+    public String deletePost(@PathVariable Long id){
+        return "Post Delete";
     }
 }
