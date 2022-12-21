c3eee120bfbf7fddc743e09927b3c0c4bfeeebb7
KoKwanwun <dkfajflsla@naver.com>
2022-12-21 15:14:12 +0900

    [Update] Swagger에 내 패키지에 있는 Controller만 보이게 수정

--- src/main/java/com/likelion/finalproject/configuration/SwaggerConfig.java
+++ src/main/java/com/likelion/finalproject/configuration/SwaggerConfig.java
@@ -8,12 +8,12 @@ import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 
 @Configuration
-public class SwaggerConfiguration {
+public class SwaggerConfig {
     @Bean
     public Docket api() {
         return new Docket(DocumentationType.OAS_30)
                 .select()
-                .apis(RequestHandlerSelectors.any())
+                .apis(RequestHandlerSelectors.basePackage("com.likelion.finalproject"))  // 내 패키지에 있는 컨트롤러만 보이게
                 .paths(PathSelectors.any())
                 .build();
     }
