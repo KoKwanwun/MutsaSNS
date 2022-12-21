234715cb041622b3b9876e4627cd4ddab6f0ae5c
KoKwanwun <dkfajflsla@naver.com>
2022-12-21 11:26:53 +0900

    [Update] User 수정 : Auditing 기능 구현

--- src/main/java/com/likelion/finalproject/domain/User.java
+++ src/main/java/com/likelion/finalproject/domain/User.java
@@ -1,6 +1,8 @@
 package com.likelion.finalproject.domain;
 
 import lombok.*;
+import org.hibernate.annotations.SQLDelete;
+import org.hibernate.annotations.Where;
 
 import javax.persistence.*;
 
@@ -9,7 +11,9 @@ import javax.persistence.*;
 @NoArgsConstructor
 @Getter
 @Builder
-public class User {
+@Where(clause = "deleted_at IS NULL")
+@SQLDelete(sql = "UPDATE user SET deleted_at = CURRENT_TIMESTAMP where id = ?")
+public class User extends BaseEntity {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
