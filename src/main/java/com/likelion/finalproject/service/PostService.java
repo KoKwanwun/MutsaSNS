46a3fa066b08fb28734bf1addebb76c38d81cc7d
KoKwanwun <dkfajflsla@naver.com>
2022-12-24 15:54:04 +0900

    [Update] post 등록, 수정 및 삭제 예외 처리 - 등록 : 유저가 DB에 존재하지 않을 경우 - 수정 및 삭제 : 유저가 DB에 존재하지 않을 경우, 유저와 작성자가 다르지만 유저의 role이 ADMIN일 경우 가능하도록

--- src/main/java/com/likelion/finalproject/service/PostService.java
+++ src/main/java/com/likelion/finalproject/service/PostService.java
@@ -1,11 +1,14 @@
 package com.likelion.finalproject.service;
 
 import com.likelion.finalproject.domain.Post;
+import com.likelion.finalproject.domain.User;
+import com.likelion.finalproject.domain.UserRole;
 import com.likelion.finalproject.domain.dto.post.PostDto;
 import com.likelion.finalproject.domain.dto.post.PostRequest;
 import com.likelion.finalproject.exception.ErrorCode;
 import com.likelion.finalproject.exception.UserException;
 import com.likelion.finalproject.repository.PostRepository;
+import com.likelion.finalproject.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.data.domain.Page;
@@ -18,8 +21,13 @@ import org.springframework.stereotype.Service;
 public class PostService {
 
     private final PostRepository postRepository;
+    private final UserRepository userRepository;
 
     public PostDto create(PostRequest request, String userName) {
+        // 작성자(유저)가 DB에 존재하지 않을 경우
+        userRepository.findByUserName(userName)
+                .orElseThrow(() -> new UserException(ErrorCode.USERNAME_NOT_FOUND, "유저 존재하지 않음"));
+
         Post savedPost = postRepository.save(PostDto.toEntity(request.getTitle(), request.getBody(), userName));
 
         return new PostDto(savedPost.getId(), savedPost.getTitle(), savedPost.getBody(),
@@ -27,7 +35,7 @@ public class PostService {
     }
 
     public PostDto printOnePost(Long postsId) {
-        // userName이 없을 경우
+        // 해당 id의 post가 없을 경우
         Post post = postRepository.findById(postsId)
                 .orElseThrow(() -> new UserException(ErrorCode.POST_NOT_FOUND, ErrorCode.POST_NOT_FOUND.getMessage()));
 
@@ -52,10 +60,12 @@ public class PostService {
         log.info("accessName : " + accessName);
         log.info("same? : " + (!post.getUserName().equals(accessName)));
 
-        // 수정자가 존재 X(추가해야 함)
+        // 유저가 존재 X(정보가 DB에 없음)
+        User user = userRepository.findByUserName(accessName)
+                .orElseThrow(() -> new UserException(ErrorCode.USERNAME_NOT_FOUND, "유저 존재하지 않음"));
 
-        // 작성자 != 수정자 (* 이후, 달라도 ROLE이 ADMIN이라면 수정가능하도록 바꿔야 함)
-        if(!post.getUserName().equals(accessName)) {
+        // 작성자 != 유저, 하지만 유저의 ROLE이 ADMIN이면 수정이나 삭제가 가능하도록
+        if(!post.getUserName().equals(accessName) && user.getRole().equals(UserRole.USER)) {
             throw new UserException(ErrorCode.INVALID_PERMISSION, ErrorCode.INVALID_PERMISSION.getMessage());
         }
 
