65d9e24a30524fe7156e77a3308005ebf6122b59
KoKwanwun <dkfajflsla@naver.com>
2022-12-26 11:44:57 +0900

    [Update] Builder 어노테이션 추가

--- src/main/java/com/likelion/finalproject/domain/dto/user/UserJoinRequest.java
+++ src/main/java/com/likelion/finalproject/domain/dto/user/UserJoinRequest.java
@@ -3,10 +3,12 @@ package com.likelion.finalproject.domain.dto.user;
 import com.likelion.finalproject.domain.User;
 import com.likelion.finalproject.domain.UserRole;
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Getter;
 
 @AllArgsConstructor
 @Getter
+@Builder
 public class UserJoinRequest {
     private String userName;
     private String password;
--- src/main/java/com/likelion/finalproject/domain/dto/user/UserLoginRequest.java
+++ src/main/java/com/likelion/finalproject/domain/dto/user/UserLoginRequest.java
@@ -1,10 +1,12 @@
 package com.likelion.finalproject.domain.dto.user;
 
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Getter;
 
 @AllArgsConstructor
 @Getter
+@Builder
 public class UserLoginRequest {
     private String userName;
     private String password;
