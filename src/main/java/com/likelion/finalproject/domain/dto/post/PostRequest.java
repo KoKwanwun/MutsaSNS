196d288d21872ae274ef3a5ed8da4d12ed6d3c5a
KoKwanwun <dkfajflsla@naver.com>
2022-12-26 13:51:26 +0900

    [Update] Builder 어노테이션 추가

--- src/main/java/com/likelion/finalproject/domain/dto/post/PostRequest.java
+++ src/main/java/com/likelion/finalproject/domain/dto/post/PostRequest.java
@@ -1,10 +1,12 @@
 package com.likelion.finalproject.domain.dto.post;
 
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Getter;
 
 @AllArgsConstructor
 @Getter
+@Builder
 public class PostRequest {
     private String title;
     private String body;
